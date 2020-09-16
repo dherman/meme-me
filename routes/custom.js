bdc7a2953e7e46539302f9263922283ee1be15d2
David Herman <david.herman@gmail.com>
2020-09-16 15:34:08 -0700

    Use Sindre's `getStream` instead of rolling my own stream concatenation logic.

--- routes/custom.js
+++ routes/custom.js
@@ -1,37 +1,14 @@
 let fs = require('fs');
 let path = require('path');
 let addon = require('../native');
-
-function onDataAll(stream) {
-  let fulfill, reject;
-  let p = new Promise((f, r) => { fulfill = f; reject = r; });
-  let chunks = [];
-
-  stream.on('data', data => {
-    chunks.push(data);
-  });
-
-  stream.on('end', () => {
-    try {
-      fulfill(Buffer.concat(chunks));
-    } catch (err) {
-      reject(err);
-    }
-  });
-
-  stream.on('error', err => {
-    reject(err);
-  });
-
-  return p;
-}
+let getStream = require('get-stream');
 
 async function routes(fastify, options) {
   fastify.route({
     url: '/custom',
     method: 'POST',
     handler: async (request, reply) => {
-      onDataAll(request.body)
+      getStream.buffer(request.body)
         .then(buffer => {
           let top = request.headers['x-meme-top-text'];
           let bottom = request.headers['x-meme-bottom-text'];
